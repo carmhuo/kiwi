5fb4ffbc072725cfe826225012e59e9585bc7bb8
e <yuanhao_huo.sgmyt@saic-gm.com>
2025-07-14 17:05:43 +0800

    refactor(backend): 重构 Kiwi 初始化脚本
    - 更新数据库连接和操作，使用 AsyncSessionLocal替代 Engine 和 Session
    -重写 init 和 main 函数，支持异步操作
    - 移除 crud.py 文件

--- backend/kiwi/initial_data.py
+++ backend/kiwi/initial_data.py
@@ -1,23 +1,52 @@
 import logging
 
-from sqlmodel import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from kiwi.core.db import engine, init_db
+
+from kiwi.core.database import AsyncSessionLocal
+from kiwi.api.schemas import UserCreate
+from kiwi.crud.user import UserCRUD
+from kiwi.core.config import settings
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def init() -> None:
-    with Session(engine) as session:
-        init_db(session)
+async def init_db(session: AsyncSession) -> None:
+    # Tables should be created with Alembic migrations
+    # But if you don't want to use migrations, create
+    # the tables un-commenting the next lines
+
+    # This works because the models are already imported and registered from kiwi.models
+    # Base.metadata.create_all(engine)
+
+    user = await UserCRUD().get_user_by_email(session, settings.FIRST_SUPERUSER)
+    if not user:
+        user_in = UserCreate(
+            username="admin",
+            email=settings.FIRST_SUPERUSER,
+            password=settings.FIRST_SUPERUSER_PASSWORD,
+            is_superuser=True,
+        )
+        user = await UserCRUD().create_user(session, user_in.model_dump())
 
 
-def main() -> None:
+async def init() -> None:
+    try:
+        async with AsyncSessionLocal() as session:
+            await init_db(session)
+    except Exception as e:
+        logger.error(e)
+        raise e
+
+
+async def main() -> None:
     logger.info("Creating initial data")
-    init()
+    await init()
     logger.info("Initial data created")
 
 
 if __name__ == "__main__":
-    main()
+    import asyncio
+
+    asyncio.run(main())
